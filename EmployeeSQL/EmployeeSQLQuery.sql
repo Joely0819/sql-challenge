c9a199dff006e1bb95f38a77c33b3a2e556b8488
Joely Alemar <joelyalemar@gmail.com>
2021-11-03 23:01:39 -0400

    Added all tables to Query

--- EmployeeSQL/EmployeeSQLQuery.sql
+++ EmployeeSQL/EmployeeSQLQuery.sql
@@ -13,7 +13,7 @@ FROM 'C:\Users\joely\Desktop\CSV_SQL\departments.csv'
 DELIMITER ',' 
 CSV HEADER;
 
---create table for employees and dept numbers
+--create table for department and employees
 create table dept_emp (
 	
 	emp_number varchar,
@@ -28,3 +28,81 @@ COPY dept_emp
 FROM 'C:\Users\joely\Desktop\CSV_SQL\dept_emp.csv'
 DELIMITER ',' 
 CSV HEADER;
+
+--create table for department and managers
+create table dept_manager (
+	
+	dept_number varchar,
+	manager_number varchar
+
+);
+select * from dept_manager;
+
+--import managers CSV
+
+COPY dept_manager
+FROM 'C:\Users\joely\Desktop\CSV_SQL\dept_manager.csv'
+DELIMITER ',' 
+CSV HEADER;
+
+--create table for employees
+create table employees (
+	
+	emp_number varchar,
+	emp_title_id varchar,
+	birth_date varchar,
+	first_name varchar,
+	last_name varchar,
+	sex varchar,
+	hire_date varchar
+	
+
+);
+select * from employees;
+
+--import employees CSV
+
+COPY employees
+FROM 'C:\Users\joely\Desktop\CSV_SQL\employees.csv'
+DELIMITER ',' 
+CSV HEADER;
+--show import
+select * from employees;
+
+--create table for salaries
+create table salaries (
+	
+	emp_number varchar,
+	salary int
+	
+
+);
+select * from salaries;
+
+--import employees CSV
+
+COPY salaries
+FROM 'C:\Users\joely\Desktop\CSV_SQL\salaries.csv'
+DELIMITER ',' 
+CSV HEADER;
+--show import
+select * from salaries;
+
+--create table for titles
+create table titles (
+	
+	title_id varchar,
+	title varchar
+	
+
+);
+select * from titles;
+
+--import employees CSV
+
+COPY titles
+FROM 'C:\Users\joely\Desktop\CSV_SQL\titles.csv'
+DELIMITER ',' 
+CSV HEADER;
+--show titles import
+select * from titles;
